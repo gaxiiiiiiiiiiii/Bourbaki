b6a31266c0d2c4f11eab772b6786985a6be9448a
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2026-01-18 21:47:08 +0900

    Finale.glue

--- Bourbaki/Topology/ch1_topology/ss2_4_finale.lean
+++ Bourbaki/Topology/ch1_topology/ss2_4_finale.lean
@@ -2,10 +2,9 @@ import Mathlib
 import Bourbaki.Topology.ch1_topology.ss1_neighbor
 import Bourbaki.Topology.ch1_topology.ss2_continuous
 import Bourbaki.Topology.ch1_topology.ss2_3_subbasis
+import Bourbaki.Topology.ch1_topology.ss2_3_init
 
 namespace Bourbaki
-
-section Def
 
 variable {X ι : Type _} {Y : ι → Type _}
 
@@ -185,39 +184,146 @@ lemma Finale.trans {X I L : Type _} {Z : I → Type _} {Y : L → Type _} [HZ : 
     apply Hg at HU
     exact HU
 
+def Finale.quotient [Topology X] (R : Setoid X) :
+  Topology (Quotient R)
+:= Finale.topology (fun (_ : PUnit.{1}) => Quotient.mk R)
+
+def Finale.sup {X I : Type _} (HX : I → Topology X) :
+  Topology X
+:= topology (HY := HX) (fun _ x => x)
+
+lemma Finale.sup_le {X I : Type _} (HX : I → Topology X) :
+  ∀ H, (∀ i, HX i ≤ H) → sup HX ≤ H
+:= by
+  intro H HH U HU
+  apply GenedOpen.base; simp [base, Finale.to]
+  intro i
+  apply HH i U HU
 
 
+lemma Finale.le_sup {X I : Type _} (HX : I → Topology X) :
+  ∀ i, HX i ≤ sup HX
+:= by
+  intro i U HU
+  unfold Topology.isOpen sup topology GenedTopology at HU; simp at HU
+  induction HU with
+  | base V HV =>
+    simp [base, Finale.to] at HV
+    exact HV i
+  | univ =>
+    apply (HX i).isOpen_univ
+  | inter S T HS HT IHS IHT =>
+    apply (HX i).isOpen_inter _ _ IHS IHT
+  | union S HS IHS =>
+    apply (HX i).isOpen_union; assumption
 
 
+def Finale.sum {I : Type _} (f : I → Type _) [Hf : ∀ i, Topology (f i)] :
+  Topology (Σ i, f i)
+:= Finale.topology (fun i => Sigma.mk i)
 
+lemma Finale.sum.isOpen_iff {I : Type _} (f : I → Type _) [Hf : ∀ i, Topology (f i)] (U : Set (Σ i, f i)) :
+  (sum f).isOpen U ↔ ∀ i, (Hf i).isOpen (Sigma.mk i ⁻¹' U)
+:= by
+  constructor<;> intro H
+  · intro i
+    have Hi := Finale.isContinuous (HY := Hf) (fun i => Sigma.mk i) i
+    rw [@IsContinuous_iff_IsOpen_preimage] at Hi
+    exact Hi U H
+  · apply GenedOpen.base
+    simp [base, Finale.to]
+    exact H
 
+def Incl {X : Type _} (S T : Set X) (ST : S ⊆ T) :
+  S ↪ T
+:= by
+  use Set.inclusion ST
+  intro ⟨x, Hx⟩ ⟨y, Hy⟩ H; grind
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-end Def
+lemma Finale.glue {X L : Type} (f : L → Set X) [Hf : ∀ l, Topology (f l)]
+  -- f l ∩ f n が、Hf l と Hf n の双方で開集合
+  (H1 : ∀ l n, {x : f l | x.val ∈ f n} ∈ (Hf l).isOpen )
+  -- f l ∩ f n ↪ f l と f l ∩ f n ↪ f n から誘導される位相は一する
+  (H2 : ∀ l n, Init.induced (Incl (f l ∩ f n) (f l) (by simp)) = Init.induced (Incl (f l ∩ f n) (f n) (by simp)))
+  :
+  let 𝓣 : Topology X := Finale.topology (HY := Hf) (fun _ => Subtype.val)
+  ∀ l, f l ∈ 𝓣.isOpen ∧ Init.induced (Function.Embedding.subtype (f l)) = Hf l
+:= by
+  intro 𝓣 l
+  constructor
+  · unfold Topology.isOpen 𝓣 topology GenedTopology; simp
+    apply GenedOpen.base; simp [base, Finale.to]
+    intro n
+    apply H1 n l
+  · apply le_antisymm; swap
+    · unfold Init.induced Init.inverse
+      apply Init.le_init; simp
+      rw [IsContinuous_iff_IsOpen_preimage]
+      intro U HU
+      induction HU with
+      | base V HV =>
+        simp [base, Finale.to] at HV
+        specialize HV l
+        exact HV
+      | univ =>
+        simp; apply (Hf l).isOpen_univ
+      | inter S T HS HT IHS IHT =>
+        simp; apply (Hf l).isOpen_inter _ _ IHS IHT
+      | union S HS IHS =>
+        simp
+        apply (Hf l).isOpen_union; simp
+        intro s
+        apply (Hf l).isOpen_union; simp
+        intro Hs
+        apply IHS s Hs
+    · intro V HV
+      apply GenedOpen.base; simp [Init.subbase, Init.to]
+      use Subtype.val '' V
+      constructor; swap; grind
+      apply GenedOpen.base; simp [base, Finale.to]
+      intro n
+      specialize H2 l n
+      set fl := (Incl (f l ∩ f n) (f l) (by simp))
+      set fn := (Incl (f l ∩ f n) (f n) (by simp))
+      have Hl := Init.isContinuous (f := fun _ : PUnit.{1} => fl) PUnit.unit
+      have Hn := Init.isContinuous (f := fun _ : PUnit.{1} => fn) PUnit.unit
+      rw [@IsContinuous_iff_IsOpen_preimage, Init.to] at Hl Hn
+      specialize Hl V HV
+      change (⇑fl ⁻¹' V) ∈ (Init.induced fl).isOpen at Hl
+      rw [H2] at Hl
+      rw [Init.isOpen_iff] at Hl ; simp at Hl
+      rcases Hl with ⟨B , HB, E⟩
+      have : Subtype.val ⁻¹' (Subtype.val '' V) = ⇑fn '' ⋃₀ B := by {
+        rw [<- E]
+        ext x; simp
+        rcases x with ⟨x, Hx⟩; simp
+        constructor<;> intro H
+        · rcases H with ⟨flx, Vx⟩
+          use x, ⟨flx, Hx⟩
+          simp [fl, fn, Incl]; assumption
+        · rcases H with ⟨y, ⟨Hly, Hny⟩, Hl, Hn⟩
+          simp [fl, fn, Incl] at Hl Hn
+          subst y
+          use Hly, Hl
+      }
+      rw [this, Set.image_sUnion]; clear this
+      apply Topology.isOpen_union; simp
+      intro b Hb; apply HB at Hb; simp at Hb
+      rcases Hb with ⟨U, HU, rfl⟩
+      induction U using Finset.cons_induction_on with
+      | empty =>
+        simp [fn, Incl]
+        apply H1
+      | cons x S Fx IH =>
+        simp; rw [Set.image_inter]; swap; grind
+        apply Topology.isOpen_inter
+        · have Hx : x ∈ Finset.cons x S Fx := by grind
+          apply HU at Hx; simp [Init.subbase, Init.to] at Hx
+          rcases Hx with ⟨V, HV, rfl⟩
+          rw [Set.image_preimage_eq_inter_range]
+          apply Topology.isOpen_inter _ _ HV
+          simp [fn, Incl]
+          apply H1
+        · apply IH
+          intro s Hs
+          apply HU; simp; grind
