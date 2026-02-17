3c32296c6c4c39700d09ccff7ec67dd4d7797744
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2026-02-17 20:25:06 +0900

    ss4

---
+++ Bourbaki/Topology/ch1_topology/ss4.lean
@@ -0,0 +1,6 @@
+import Bourbaki.Topology.ch1_topology.ss4_1_prod
+import Bourbaki.Topology.ch1_topology.ss4_1_prod'
+import Bourbaki.Topology.ch1_topology.ss4_2_section
+import Bourbaki.Topology.ch1_topology.ss4_2_section'
+import Bourbaki.Topology.ch1_topology.ss4_3_closure
+import Bourbaki.Topology.ch1_topology.ss4_4_projLimit
