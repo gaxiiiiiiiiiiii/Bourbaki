cf925a486e417ba9f459d3b21d88bb3c5ab7d918
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2026-02-20 07:07:51 +0900

    abbrev

--- Bourbaki/Set/ch3_Order/InvSystem.lean
+++ Bourbaki/Set/ch3_Order/InvSystem.lean
@@ -13,7 +13,6 @@ abbrev InvSystem (I 𝓒) [Preorder I] [Category 𝓒] := Iᵒᵖ ⥤ 𝓒
 def InvSystem.proj [Preorder I] [Category 𝓒] (S : InvSystem I 𝓒) := S.obj
 
 
-@[simp]
 def InvSystem.mapOf [Preorder I] [Category 𝓒] {S : InvSystem I 𝓒} {i j : I} (H : i ≤ j) :
   S.obj (op j) ⟶ S.obj (op i)
 := S.map (homOfLE H).op
@@ -21,12 +20,12 @@ def InvSystem.mapOf [Preorder I] [Category 𝓒] {S : InvSystem I 𝓒} {i j : I
 @[simp]
 lemma InvSystem.mapOf_id [Preorder I] [Category 𝓒] {S : InvSystem I 𝓒} {i : I} :
   S.mapOf (le_refl i) = 𝟙 (S.obj (op i))
-:= by simp
+:= by simp [mapOf]
 
 lemma InvSystem.mapOf_comp [Preorder I] [Category 𝓒] {S : InvSystem I 𝓒} {i j k : I} (Hij : i ≤ j) (Hjk : j ≤ k) :
   S.mapOf (le_trans Hij Hjk) = S.mapOf Hjk ≫ S.mapOf Hij
 := by
-  simp; rw [<- S.map_comp]; congr
+  simp [mapOf]; rw [<- S.map_comp]; congr
 
 
 lemma InvSystem.naturality [Preorder I] [Category 𝓒] {X Y : InvSystem I 𝓒} (σ : X ⟶ Y) {i j : I} (H : i ≤ j) :
@@ -38,13 +37,13 @@ section
 
 variable [Preorder I] [Category 𝓒] (S : InvSystem I 𝓒) [HasLimit S]
 
-noncomputable def InvSystem.limit : 𝓒 := Limits.limit S
+noncomputable abbrev InvSystem.limit : 𝓒 := Limits.limit S
 
 noncomputable def InvSystem.π :
   (i : I) → limit S ⟶ S.obj (op i)
 := fun i => Limits.limit.π S (op i)
 
-noncomputable def InvSystem.lift  (c : Cone S) :
+noncomputable abbrev InvSystem.lift  (c : Cone S) :
   c.pt ⟶ limit S
 := Limits.limit.lift S c
 
@@ -73,4 +72,3 @@ lemma InvSystem.limMap_ext [Preorder I] [Category 𝓒] {S : InvSystem I 𝓒} {
   (f f' : S.limit ⟶ T.limit) (H : ∀ (i : I), f ≫ T.π i = f' ≫ T.π i) :
   f = f'
 := hom_ext _ _ _ H
-
