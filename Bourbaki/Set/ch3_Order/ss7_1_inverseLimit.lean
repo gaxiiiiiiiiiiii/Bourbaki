d852b831e0484aa133d9b634a9a329f0b71f5ed8
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2026-02-23 22:56:38 +0900

    InvSystem.restrict

--- Bourbaki/Set/ch3_Order/ss7_1_inverseLimit.lean
+++ Bourbaki/Set/ch3_Order/ss7_1_inverseLimit.lean
@@ -6,39 +6,42 @@ open Limits
 
 namespace Bourbaki
 
-def InvSystem.restrict [Preorder I] [Category 𝓒] (S : InvSystem I 𝓒) (J : Set I):
-  InvSystem J 𝓒
-where
-  obj i := S.obj (op i.unop.val)
-  map {i j } f := by
-    apply S.map
-    exact WithTerminal.down f
-  map_id := by
-    intro i
-    simp [WithTerminal.down]
-    exact S.map_id (op ↑(unop i))
-  map_comp := by
-    intro i j k f g
-    simp [WithTerminal.down]
-    apply S.map_comp
+instance [Preorder I] (J : MonoOver I) :
+  Preorder J.obj.left
+:= Preorder.lift J.arrow
 
-noncomputable def InvSystem.subMap [Preorder I] [Category 𝓒] {S : InvSystem I 𝓒} (J : Set I) [HasLimit S] [HasLimit (S.restrict J)] :
-  S.limit ⟶ (S.restrict J).limit
+instance [Preorder I] (J : MonoOver I) :
+  Category (J.obj.left)
+:= Preorder.smallCategory J.obj.left
+
+def _root_.CategoryTheory.MonoOver.functor [Preorder I] (J : MonoOver I) :
+  J.obj.left ⥤ I
+:= {
+  obj i := J.arrow i
+  map  f :=  ⟨⟨f.down.down⟩⟩
+  map_id _ :=  Eq.symm (eq_of_comp_right_eq fun {_} ↦ congrFun rfl)
+  map_comp _ _ := Eq.symm (eq_of_comp_right_eq fun {_} ↦ congrFun rfl)
+}
+
+
+def InvSystem.restrict [Preorder I] [Category 𝓒] (E : InvSystem I 𝓒) (J : MonoOver I) :
+  InvSystem J.obj.left 𝓒
+:= J.functor.op ⋙ E
+
+noncomputable def InvSystem.restrict.limMap [Preorder I] [Category 𝓒] {E : InvSystem I 𝓒} (J : MonoOver I) [HasLimit E] [HasLimit (E.restrict J)] :
+  E.limit ⟶ (E.restrict J).limit
 := by
-  let c : Cone (S.restrict J) := {
-    pt := S.limit
+  let c : Cone (E.restrict J) := {
+    pt := E.limit
     π := {
       app i := by
-        simp [InvSystem.restrict]
-        exact  S.π i.unop.val
-      naturality := by
-        intro i j f
-        simp [InvSystem.restrict, WithTerminal.down]
-        have H : j.unop.val ≤ i.unop.val := by
-          simp; exact f.unop.down.down
-        rw [<- S.w H]; rfl
+        simp [InvSystem.restrict, MonoOver.functor]
+        exact E.π (J.arrow i.unop)
+      naturality {i j} f := by
+        simp [InvSystem.restrict, MonoOver.functor, InvSystem.π]
     }
   }
-  apply (S.restrict J).lift c
+  exact (E.restrict J).lift c
+
 
 end Bourbaki
