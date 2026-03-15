7da3cd5c3c406af7ad339f3796627c93fb2c196d
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2026-03-16 07:23:49 +0900

    projLimit

--- Bourbaki/Topology/ch1_topology/ss4_4_projLimit.lean
+++ Bourbaki/Topology/ch1_topology/ss4_4_projLimit.lean
@@ -5,33 +5,74 @@ import Bourbaki.Topology.ch1_topology.ss4_1_prod
 import Bourbaki.Topology.ch1_topology.ss4_1_prod'
 import Bourbaki.Topology.ch1_topology.ss4_2_section
 import Bourbaki.Topology.ch1_topology.ss4_3_closure
+import Bourbaki.Set.ch3_Order.ss7_1_inverseLimit
+import Bourbaki.Set.ch3_Order.ss7_2_map
 
 
 
 
 
 namespace Bourbaki
-
-structure InvSystem [Preorder I] (X : I → Type k)  where
-  map {i j} : i ≤ j →  X j → X i
-  map_id : ∀ i, map (le_refl i) = id
-  map_comp : ∀ {i j k} (Hij : i ≤ j) (Hjk : j ≤ k),
-     map Hij ∘ map Hjk = map (le_trans Hij Hjk)
-
-def InvSystem.limit [Preorder I] {X : I → Type _}  (S : InvSystem X) :=
-  {f : Π i, X i | ∀ i j (H : i ≤ j), f i = S.map H (f j)}
-
-def InvSystem.proj  [Preorder I] {X : I → Type _} (S : InvSystem X) (i : I) :
-  limit S → X i
-:= fun f => f.val i
+open CategoryTheory
+open Opposite
+open Limits
 
 
-structure InvSystem.isContinuous [Preorder I] {X : I → Type _} [∀ i, Topology (X i)] (S : InvSystem X) : Prop where
-  continuous : ∀ {i j} (Hij : i ≤ j), IsContinuous (S.map Hij)
-
-def InvSystem.topology {I : Type _} [Preorder I] {X : I → Type _} [∀ i, Topology (X i)] (S : InvSystem X) :
-  Topology (limit S)
-:= Init.topology (proj S)
+structure TOP where
+  carrier : Type _
+  isTopology : Topology carrier
 
 
---TODO 部分の逆系について
+instance : CoeSort TOP (Type _) := ⟨TOP.carrier⟩
+
+instance (X : TOP) : Topology X := X.isTopology
+
+
+structure TOP.Hom (X Y : TOP) where
+  toFun : X.carrier → Y.carrier
+  isContinuous : IsContinuous toFun
+
+
+instance Topology.category :
+  Category TOP
+where
+  Hom X Y := TOP.Hom X Y
+  id X := {
+    toFun := id
+    isContinuous := by
+      intro x S; simp
+  }
+  comp {X Y Z} f g := {
+    toFun := g.toFun ∘ f.toFun
+    isContinuous := IsContinuous_comp f.isContinuous g.isContinuous
+  }
+
+
+instance (X Y : TOP) : CoeFun (X ⟶ Y) (fun _ => X.carrier → Y.carrier) := ⟨TOP.Hom.toFun⟩
+
+
+
+
+def InvSystem.topology [SmallCategory I] (E : InvSystem I TOP) [HasLimit E] :
+  Topology (↑(limit E))
+:= Init.topology  (fun (i : Iᵒᵖ) (x : (limit E).carrier) => limit.π E i x)
+
+
+def InvSystem.Sub.limSet [SmallCategory I] {E : InvSystem I TOP} [HasLimit E]
+  (F : E.Sub) [HasLimit F.obj.left] :
+  Set (limit E).carrier
+:= Set.range (Limits.limMap F.arrow)
+
+def InvSystem.Sub.topology
+  [SmallCategory I] {E : InvSystem I TOP} [HasLimit E]
+  (F : E.Sub) [HasLimit F.obj.left]
+  : Topology ↑F.limSet
+:= Subtopology (F.limSet)
+
+
+
+noncomputable example
+  [SmallCategory I] (X  : InvSystem I TOP) [HasLimit X]
+  (J : SubIndex I) [HJ : J.functor.op.Initial] [HasLimit (X.restrict J)] :
+  limit (X.restrict J) ≅ limit X
+:= InvSystem.restrict.limitIso X J
